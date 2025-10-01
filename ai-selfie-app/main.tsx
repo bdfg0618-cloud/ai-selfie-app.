c646be3186442062623043995a43332bcb0afcef
bdfg0618-cloud <bdfg0618@gmail.com>
2025-10-01 13:19:14 +0900

    Update and rename index.tsx.html to main.tsx

---
+++ ai-selfie-app/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found. Did you forget <div id='root'></div> in index.html?");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
